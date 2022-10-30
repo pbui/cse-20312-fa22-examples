078fa6fdd072000c35599a66ad4e0f0bab7a3073
Peter Bui <pbui@nd.edu>
2022-10-30 13:50:51 -0400

    Lecture 22: add tree_read

---
+++ lecture22/tree_read.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+''' tree_read.py
+
+Read in tree in BFS format using divide and conquer.
+
+'''
+
+from dataclasses import dataclass
+import sys
+
+# Classes
+
+@dataclass
+class Node:
+    value:  int
+    left:   'Node' = None
+    right:  'Node' = None
+
+# Functions
+
+def tree_read(array, index=0):
+    ''' Use divide and conquer to read binary tree in BFS format '''
+    # Base Case: Invalid Node
+    if index >= len(array):
+        return None
+
+    # Divide and Conquer and Combine
+    return Node(
+        value = array[index],
+        left  = tree_read(array, 2*index + 1),
+        right = tree_read(array, 2*index + 2),
+    )
+
+# Main Execution
+
+def main():
+    for line in sys.stdin:
+        strs = line.split()             # Separate individual strings in line
+        ints = [int(s) for s in strs]   # Convert each string into an int and collec into list
+        tree = tree_read(ints)
+
+        print(tree)
+
+if __name__ == '__main__':
+    main()
