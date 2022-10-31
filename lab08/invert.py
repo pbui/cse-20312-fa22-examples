be283c040a442338b86f786305584fee4acd802c
Peter Bui <pbui@nd.edu>
2022-10-30 20:39:27 -0400

    Lab 08: add invert and tree skeletons

---
+++ lab08/invert.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+'''
+Title:      invert.py
+Abstract:   Invert binary tree.
+Author:     Peter Bui
+Email:      pbui@nd.edu
+Estimate:   25 minutes
+Date:       11/01/2022
+Questions:
+
+    1. What is the worst-case time complexity of tree_invert()?
+
+        O(n)
+
+    2. How do you swap two values in Python without a temporary value?
+
+        a, b = b, a
+'''
+
+import sys
+
+# Functions
+
+def tree_invert(node):
+    ''' Invert tree in-place using divide and conquere.
+
+    >>> tree_walk(tree_invert(tree_read([1, 2, 3])))
+    1, 3, 2
+
+    >>> tree_walk(tree_invert(tree_read([1, 2, 3, 4, 0, 0, 6])))
+    1, 3, 2, 6, 0, 0, 4
+    '''
+    pass
+
+# Main Execution
+
+def main(stream=sys.stdin):
+    ''' For each line, read in the tree in BFS format, print it, invert it, and
+    then print it again.
+
+    >>> import io
+    >>> main(io.StringIO('1 2 3\\n1 2 3 4 0 0 6\\n'))
+    1, 2, 3
+    1, 3, 2
+    1, 2, 3, 4, 0, 0, 6
+    1, 3, 2, 6, 0, 0, 4
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ lab08/tree.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+'''
+Title:      tree.py
+Abstract:   Implement a binary tree read and walk functions.
+Author:     Peter Bui
+Email:      pbui@nd.edu
+Estimate:   25 minutes
+Date:       11/01/2022
+Questions:
+
+    1. What is the worst-case time complexity of tree_read()?
+
+        ????
+
+    2. What is the worst-case time complexity of tree_walk()?
+
+        ????
+
+    3. In tree_walk(), how did you modify BFS to print all the nodes on one
+    comma separated line?
+
+        ????
+
+    4. In tree_walk(), how did you remove any trailing invalid nodes from your
+    output?
+
+        ????
+'''
+
+from dataclasses import dataclass
+from collections import deque
+
+# Classes
+
+@dataclass
+class Node:
+    pass
+
+# Functions
+
+def tree_read(array, index=0):
+    ''' Return a node-based tree from the given array of values in BFS format.
+
+    >>> tree_read([1, 2, 3])
+    Node(value=1, left=Node(value=2, left=None, right=None), right=Node(value=3, left=None, right=None))
+
+    >>> tree_read([1, 2, 3, 4, 0, 0, 6])
+    Node(value=1, left=Node(value=2, left=Node(value=4, left=None, right=None), right=None), right=Node(value=3, left=None, right=Node(value=6, left=None, right=None)))
+    '''
+    pass
+
+def tree_walk(root):
+    ''' Print out the tree in level-by-level order with each level on the same
+    line.
+
+    >>> tree_walk(tree_read([1, 2, 3]))
+    1, 2, 3
+    >>> tree_walk(tree_read([1, 2, 3, 4, 0, 0, 6]))
+    1, 2, 3, 4, 0, 0, 6
+    '''
+    pass
